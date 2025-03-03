bcb374d8b5dc352b18a40aa0f8e17cb185d6cc44
Samuel <samuelbaraujo19@gmail.com>
2025-03-03 15:47:40 -0300

    Will add features later for the new buttons ( stopwatch and insert time ). Disabled copy and paste, imported the new javascript files

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,32 @@
-import { timer } from "./timer.js";
+import { timer, finishTimer } from "./timer.js";
+import { charactersTyped } from "./characters.js";
+import { grossWpm } from "./grosswpm.js";
 const inputTypingTest = document.querySelector("#input-typing-test");
-const finishTimer = document.querySelector("#finish-timer");
+const buttonFinishTimer = document.querySelector("#finish-timer");
+const buttonInsertTime = document.querySelector("#insert-time-button");
+const buttonStopWatch = document.querySelector("#stopwatch-button");
+
+inputTypingTest.addEventListener("paste", (event) => event.preventDefault());
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
 timer();
 
-finishTimer.addEventListener("click", () => {
-  console.log(timer());
+buttonInsertTime.addEventListener("click", () => {
+  console.log("insert");
+});
+
+buttonStopWatch.addEventListener("click", () => {
+  console.log("stopwatch");
+});
+
+buttonFinishTimer.addEventListener("click", () => {
+  finishTimer();
+  grossWpm();
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && event.shiftKey) {
+    finishTimer();
+    grossWpm();
+  }
 });
