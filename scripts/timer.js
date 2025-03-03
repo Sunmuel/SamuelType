bc6712440920bbf47ff0d31f529666fd5fc6f498
Samuel <samuelbaraujo19@gmail.com>
2025-03-03 10:04:04 -0300

    Finished the timer that counts up until someone clikc the finish button

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,29 +1,40 @@
 const showWpmResult = document.querySelector("#show-wpm-result");
+const finishTimer = document.querySelector("#finish-timer");
+let idInterval;
+
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let time = 0;
+idInterval = setInterval(() => {
+  if (seconds < 59) {
+    seconds++;
+  } else {
+    if (minutes < 59) {
+      minutes++;
+    } else {
+      if (hours < 24) {
+        hours++;
+      } else {
+        alert("please stop typing bruh");
+      }
+      minutes = 0;
+    }
+    seconds = 0;
+  }
+
+  time = hours * 3600 + minutes * 60 + seconds;
+
+  showWpmResult.innerHTML = `${hours}:${String(minutes).padStart(
+    2,
+    "0"
+  )}:${String(seconds).padStart(2, "0")}`;
+}, 1000);
+
+finishTimer.addEventListener("click", () => {
+  clearInterval(idInterval);
+});
 
 export function timer() {
-  let seconds = 0;
-  let minutes = 0;
-  let hours = 0;
-  setInterval(() => {
-    if (seconds < 60) {
-      seconds++;
-    } else {
-      if (minutes < 60) {
-        minutes++;
-      } else {
-        if (hours < 24) {
-          hours++;
-        } else {
-          console.log("please stop typing nigga");
-        }
-        minutes = 0;
-      }
-      seconds = 0;
-    }
-
-    showWpmResult.innerHTML = `${hours}:${String(minutes).padStart(
-      "2",
-      0
-    )}:${String(seconds).padStart("2", 0)}`;
-  }, 1000);
+  return time;
 }
