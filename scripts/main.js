5d1271d34609c6dab3afbbcdf96a87d908b33894
Samuel <samuelbaraujo19@gmail.com>
2025-03-04 18:36:58 -0300

    Imported some new functions from timer.js, added some new DOMs. Started to check if an option is already in use, to prevent others for example. Started to configure the insert time and stopwatch options, being the stopwatch option basically done ( new featured going to be added in the future ). Configured an alert if you start to type in the input without having chosed any option

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,85 @@
-import { timer, finishTimer } from "./timer.js";
+import { stopwatch, timerRunning, clearTime, insertTime } from "./timer.js";
 import { charactersTyped } from "./characters.js";
 import { grossWpm } from "./grosswpm.js";
+
 const inputTypingTest = document.querySelector("#input-typing-test");
-const buttonFinishTimer = document.querySelector("#finish-timer");
+const inputContainer = document.querySelector("#input-container");
+const insertTimeContainer = document.querySelector("#insert-time-container");
 const buttonInsertTime = document.querySelector("#insert-time-button");
 const buttonStopWatch = document.querySelector("#stopwatch-button");
-const containerInsertTimer = document.querySelector("#insert-time-container");
+const showTimer = document.querySelector("#show-timer");
+const selectAnOptionAlert = document.querySelector("#select-an-option-alert");
 
 inputTypingTest.addEventListener("paste", (event) => event.preventDefault());
 document.addEventListener("contextmenu", (event) => event.preventDefault());
 
+let insertTimePressed = false;
+let stopwatchPressed = false;
+
+buttonInsertTime.classList.add("insert-time-button-unpressed");
 buttonInsertTime.addEventListener("click", () => {
-  containerInsertTimer.innerHTML += `
-    <input id="insert-time-input" type="number">
-    <button id="insert-button">Insert</button>
-  `;
-  const insertTimeInput = document.querySelector("#insert-time-input");
-  const insertButton = document.querySelector("#insert-button");
-  insertButton.addEventListener("click", () => {
-    const timeInserted = insertTimeInput.value;
-    console.log(timeInserted);
-  });
+  if (!insertTimePressed) {
+    insertTimePressed = true;
+    stopwatchPressed = false;
+    buttonInsertTime.classList.remove("insert-time-button-unpressed");
+
+    showTimer.innerHTML = "";
+
+    if (inputTypingTest.value) {
+      inputTypingTest.value = "";
+    }
+
+    insertTimeContainer.innerHTML += `
+      <input type="number" id="insert-time-input" autocomplete="off" />
+      <button id="insert-button">Insert</button>
+    `;
+
+    const insertTimeInput = document.querySelector("#insert-time-input");
+    const insertButton = document.querySelector("#insert-button");
+
+    insertButton.addEventListener("click", () => {
+      const timeInserted = insertTimeInput.value;
+      insertTime(timeInserted);
+    });
+  }
 });
 
 buttonStopWatch.addEventListener("click", () => {
-  timer();
+  if (!timerRunning && !stopwatchPressed) {
+    stopwatchPressed = true;
+    insertTimePressed = false;
+
+    showTimer.innerHTML = "0:00:00";
+    insertTimeContainer.innerHTML = "";
+    buttonInsertTime.classList.add("insert-time-button-unpressed");
+
+    inputTypingTest.addEventListener("input", () => {
+      stopwatch();
+    });
+
+    if (inputTypingTest.value) {
+      inputTypingTest.value = "";
+    }
+
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "Enter" && event.shiftKey) {
+        stopwatchPressed = false;
+        grossWpm();
+        clearTime();
+      }
+    });
+  }
 });
 
-buttonFinishTimer.addEventListener("click", () => {
-  grossWpm();
-});
-
-window.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" && event.shiftKey) {
-    grossWpm();
+let alertTimeout = false;
+inputTypingTest.addEventListener("input", () => {
+  if (!insertTimePressed && !stopwatchPressed && !alertTimeout) {
+    alertTimeout = true;
+    selectAnOptionAlert.innerHTML = `&#9888;&#65039 You should select an option`;
+    setTimeout(() => {
+      inputTypingTest.value = "";
+      selectAnOptionAlert.innerHTML = "";
+      alertTimeout = false;
+    }, 3000);
   }
 });
