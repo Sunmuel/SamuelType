63720dda166d7938a18cc2d001b5d4a29a1356d0
Samuel <samuelbaraujo19@gmail.com>
2025-03-03 22:56:06 -0300

    Started to make some features for the two new button, insert time and stopwatch, and removed the finishTimer() function when you finish the timer, because grossWpm() already calls the finishTimer() function

--- scripts/main.js
+++ scripts/main.js
@@ -5,28 +5,34 @@ const inputTypingTest = document.querySelector("#input-typing-test");
 const buttonFinishTimer = document.querySelector("#finish-timer");
 const buttonInsertTime = document.querySelector("#insert-time-button");
 const buttonStopWatch = document.querySelector("#stopwatch-button");
+const containerInsertTimer = document.querySelector("#insert-time-container");
 
 inputTypingTest.addEventListener("paste", (event) => event.preventDefault());
 document.addEventListener("contextmenu", (event) => event.preventDefault());
 
-timer();
-
 buttonInsertTime.addEventListener("click", () => {
-  console.log("insert");
+  containerInsertTimer.innerHTML += `
+    <input id="insert-time-input" type="number">
+    <button id="insert-button">Insert</button>
+  `;
+  const insertTimeInput = document.querySelector("#insert-time-input");
+  const insertButton = document.querySelector("#insert-button");
+  insertButton.addEventListener("click", () => {
+    const timeInserted = insertTimeInput.value;
+    console.log(timeInserted);
+  });
 });
 
 buttonStopWatch.addEventListener("click", () => {
-  console.log("stopwatch");
+  timer();
 });
 
 buttonFinishTimer.addEventListener("click", () => {
-  finishTimer();
   grossWpm();
 });
 
 window.addEventListener("keydown", (event) => {
   if (event.key === "Enter" && event.shiftKey) {
-    finishTimer();
     grossWpm();
   }
 });
