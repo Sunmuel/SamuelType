3ef0014ced451ec7c05119934f6d371fbed43e70
Samuel <samuelbaraujo19@gmail.com>
2025-03-05 23:58:57 -0300

    Made the insert customized timer option, where you can define how much time you are going to spend typing, a countdown where the used chooses the countdown. Isn't entirely done yet.

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,45 +1,70 @@
 const showTimer = document.querySelector("#show-timer");
 const buttonFinishTimer = document.querySelector("#finish-timer");
+const inputTypingTest = document.querySelector("#input-typing-test");
 
 let idInterval;
 let idDisplayInterval;
 let insertTimeIntervalId;
-export let timerRunning = false;
 
-let seconds = 0;
+export let stopwatchRunning = false;
+export let timeInsertedRunning = false;
+
+let hours = 0;
 let minutes = 0;
+let seconds = 0;
 let tenMilliseconds = 0;
 
+let displayMilliseconds = 0;
 let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
 
 export function stopwatch() {
-  if (timerRunning === false) {
-    timerRunning = true;
+  if (stopwatchRunning === false) {
+    stopwatchRunning = true;
     idDisplayInterval = setInterval(() => {
-      if (displaySeconds < 59) {
-        displaySeconds++;
+      if (displayMilliseconds < 100) {
+        displayMilliseconds++;
       } else {
-        if (displayMinutes < 59) {
-          displayMinutes++;
+        if (displaySeconds < 59) {
+          displaySeconds++;
         } else {
-          if (displayHours < 24) {
-            displayHours++;
+          if (displayMinutes < 59) {
+            displayMinutes++;
           } else {
-            alert("bruh, stop typing mate, please");
-            displayHours = 0;
+            if (displayHours < 24) {
+              displayHours++;
+            } else {
+              alert("bruh, stop typing mate, please");
+              displayHours = 0;
+            }
+            displayMinutes = 0;
           }
-          displayMinutes = 0;
+          displaySeconds = 0;
         }
-        displaySeconds = 0;
+        displayMilliseconds = 0;
       }
 
-      showTimer.innerHTML = `${displayHours}:${String(displayMinutes).padStart(
-        2,
-        "0"
-      )}:${String(displaySeconds).padStart(2, "0")}`;
-    }, 1000);
+      if (displayHours >= 1) {
+        showTimer.innerHTML = `${displayHours}:${String(
+          displayMinutes
+        ).padStart(2, "0")}:${String(displaySeconds).padStart(2, "0")}:${String(
+          displayMilliseconds
+        ).padStart(2, "0")}`;
+      } else if (displayMinutes >= 1 && displayHours === 0) {
+        showTimer.innerHTML = `${String(displayMinutes).padStart(
+          2,
+          "0"
+        )}:${String(displaySeconds).padStart(2, "0")}:${String(
+          displayMilliseconds
+        ).padStart(2, "0")}`;
+      } else if (displayHours === 0 && displayMinutes === 0) {
+        showTimer.innerHTML = `${String(displaySeconds).padStart(
+          2,
+          "0"
+        )}:${String(displayMilliseconds).padStart(2, "0")}`;
+      }
+    }, 10);
 
     idInterval = setInterval(() => {
       tenMilliseconds++;
@@ -50,49 +75,45 @@ export function stopwatch() {
 }
 
 export function insertTime(timeInserted) {
-  // insertTimeIntervalId = setInterval(() => {
-  //   seconds = timeInserted;
-  //   if (timeInserted === 0) {
-  //     alert("The time finished");
-  //     clearInterval(insertTimeIntervalId);
-  //   } else {
-  //     timeInserted--;
-  //     if (seconds > 0 && seconds < 59) {
-  //       displaySeconds =
-  //     } else if (seconds) {
-  //     }
-  //     showTimer.innerHTML = ``;
-  //   }
-  // }, 1000);
-  // ===========================================================
-  // ===========================================================
-  // ===========================================================
-  // let number = 7321;
-  // let hours = 0;
-  // let minutes = 0;
-  // let seconds = 0;
-  // hours = (number - (number % 3600)) / 3600;
-  // number %= 3600;
-  // minutes = (number - (number % 60)) / 60;
-  // number %= 60;
-  // seconds = number;
-  // showTimer.innerHTML = `${hours}:${String(minutes).padStart(2, "0")}:${String(
-  //   seconds
-  // ).padStart(2, "0")}`;
-  // setInterval(() => {
-  //   number--;
-  //   if (number / 3600 > 0) {
-  //   }
-  //   console.log(number);
-  //   console.log(hours);
-  // }, 1000);
+  hours = (timeInserted - (timeInserted % 3600)) / 3600;
+  minutes = timeInserted % 3600;
+  minutes = (minutes - (minutes % 60)) / 60;
+  seconds = timeInserted % 60;
+
+  showTimer.innerHTML = `${hours}:${String(minutes).padStart(2, "0")}:${String(
+    seconds
+  ).padStart(2, "0")}`;
+
+  inputTypingTest.addEventListener("input", () => {
+    if (!timeInsertedRunning) {
+      timeInsertedRunning = true;
+      insertTimeIntervalId = setInterval(() => {
+        if (timeInserted > 0) {
+          timeInserted--;
+        } else {
+          clearInterval(insertTimeIntervalId);
+          timeInsertedRunning = false;
+        }
+
+        hours = (timeInserted - (timeInserted % 3600)) / 3600;
+        minutes = timeInserted % 3600;
+        minutes = (minutes - (minutes % 60)) / 60;
+        seconds = timeInserted % 60;
+
+        showTimer.innerHTML = `${hours}:${String(minutes).padStart(
+          2,
+          "0"
+        )}:${String(seconds).padStart(2, "0")}`;
+      }, 1000);
+    }
+  });
 }
 
 export function finishStopwatch() {
   clearInterval(idInterval);
   clearInterval(idDisplayInterval);
 
-  timerRunning = false;
+  stopwatchRunning = false;
 
   displaySeconds = 0;
   displayMinutes = 0;
